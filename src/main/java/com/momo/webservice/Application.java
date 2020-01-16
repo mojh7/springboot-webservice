b065c0adb9436f337eb7891e32dac27fcc53c32b
momo <mjh79017901@gmail.com>
2020-01-16 15:28:14 +0900

    운영 환경의 yml 프로젝트 외부 생성 후 테스트

--- src/main/java/com/momo/webservice/Application.java
+++ src/main/java/com/momo/webservice/Application.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) { SpringApplication.run(Application.class, args); }
 }
